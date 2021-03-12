e73e824f009d19372bf0de9d5a03d80149ca2522
RM <70502707+reyad0606@users.noreply.github.com>
2021-03-12 01:18:04 -0500

    final
    increment counter

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(42);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className='App'>
+      <h2 className='counter'>Counter</h2>
+      <span>{count}</span>
+      <button className='counter-button' onClick={incrementCount}>
+        Click
+      </button>
+    </div>
+  );
+}
+
+export default App;
